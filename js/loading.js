c56035a4f01601b6a6f756ebacaa604b3c53c6cd
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-02-27 18:05:55 +0000

    Tue, Feb 27, 2024, 10:05 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -13,8 +13,7 @@ function blurring() {
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
-
-
     loadText.style.opacity = 1 - load/100
-    bg.style.filter = "blur(10px)"
+
+    bg.style.filter = `blur(${30 - load}px)`
 }
