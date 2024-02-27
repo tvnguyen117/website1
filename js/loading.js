2d061a2aa704d63f542b00b623622fc88b3806d0
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-02-27 17:57:31 +0000

    Tue, Feb 27, 2024, 9:57 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,16 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
-loadText.innerText = "7%"
+blurring()
+blurring()
+blurring()
+
+
+function blurring() {
+    load = load + 1
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
