8c6c6ecd26a9cec782ca67ac50be61f9f64b4ce9
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-02-28 17:57:16 +0000

    Wed, Feb 28, 2024, 9:57 AM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,6 +2,7 @@ panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click, () => {
+        removeActiveClasses()
         panel.classList.add('active')
     })
 })
