88e58cee6d9d97df0022809fd3c73b917546c92f
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-03-11 16:55:48 +0000

    Mon, Mar 11, 2024, 9:55 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,6 +1,12 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElemetById('modal')
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
 })
