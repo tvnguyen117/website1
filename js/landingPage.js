f0e072394b51d1bd81739aeb1a167d5f661954e9
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-03-11 17:06:46 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -3,15 +3,24 @@ open = document.getElementById('open')
 modal = document.getElemetById('modal')
 close = document.getElementById('close')
 
-
+// Toggle Nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+// Show Modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+// Hide Modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//Hide Modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target== modal) {
+        modal.classList.remove('show-modal')
+    }
 })
